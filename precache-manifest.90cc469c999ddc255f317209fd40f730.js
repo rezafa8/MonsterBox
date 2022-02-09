8f202d134d6c33d8d3ecfa837564a5cc8b2872ac
rezafa8 <danahanseb@gmail.com>
2022-02-09 02:58:42 +0100

    Updates

---
+++ precache-manifest.90cc469c999ddc255f317209fd40f730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9919970840a5f324382d9a083cd5c0ad",
+    "url": "/MonsterBox/index.html"
+  },
+  {
+    "revision": "df0ac81284850462de1b",
+    "url": "/MonsterBox/static/css/main.5ce52e3c.chunk.css"
+  },
+  {
+    "revision": "42d8e8859104679ce821",
+    "url": "/MonsterBox/static/js/2.bc0e5e72.chunk.js"
+  },
+  {
+    "revision": "dfa8c23334d711f8ea6c",
+    "url": "/MonsterBox/static/js/3.fb07916a.chunk.js"
+  },
+  {
+    "revision": "df0ac81284850462de1b",
+    "url": "/MonsterBox/static/js/main.1d67c9a3.chunk.js"
+  },
+  {
+    "revision": "1e6303cfa54f768a29d7",
+    "url": "/MonsterBox/static/js/runtime~main.8c33d475.js"
+  }
+]);
